ce691a48818d60a30f60c157537429e8e50f6d4e
John Evans <lgastako@gmail.com>
2014-04-14 18:21:12 -0700

    mo

--- project.clj
+++ project.clj
@@ -4,34 +4,35 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :min-lein-version "2.1.3"  ; added :global-vars
-  :dependencies [[com.stuartsierra/dependency "0.1.1"]]
+  :dependencies [[com.stuartsierra/dependency "0.1.2.x1-SNAPSHOT"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [org.clojure/clojurescript "0.0-2197"]
                                   [org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}
              :clj1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :plugins [[com.keminglabs/cljx "0.3.2" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.0"]]
-  :source-paths ["src" "target/gen/src"]
-  :test-paths ["test" "target/gen/test"]
+  :hooks [leiningen.cljsbuild cljx.hooks]
+  :source-paths ["target/gen/src"]
+  :test-paths ["target/gen/test"]
   :cljx {:builds [{:source-paths ["src"]  :output-path "target/gen/src"  :rules :clj}
                   {:source-paths ["src"]  :output-path "target/gen/src"  :rules :cljs}
                   {:source-paths ["test"] :output-path "target/gen/test" :rules :clj}
                   {:source-paths ["test"] :output-path "target/gen/test" :rules :cljs}]}
-  :cljsbuild {:builds {:dev {:source-paths ["src" "gen/src"]
+  :cljsbuild {:builds {:dev {:source-paths ["target/gen/src"]
                              :compiler {:output-to "target/gen/js/main.js"
                                         :output-dir "target/gen/js"
                                         :optimizations :whitespace
                                         :pretty-print true
                                         :source-map "target/gen/js/main.js.map"}}
-                       :test {:source-paths ["src" "test"
-                                             "target/gen/src" "target/gen/test"]
-                              :compiler {:output-to "gen/test/js/testable.js"
+                       :test {:source-paths ["target/gen/src" "target/gen/test"]
+                              :compiler {:output-to "target/gen/test/js/testable.js"
                                          ;; :optimizations :advanced
                                          :optimizations :simple
                                          :pretty-print true}}}
               :test-commands {"phantomjs" ["phantomjs"
                                            :runner
                                            "bind-polyfill.js"
-                                           "gen/test/js/testable.js"]}})
+                                           "target/gen/test/js/testable.js"]}})
